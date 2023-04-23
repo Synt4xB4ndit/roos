178bd1c2d4d68e7704c347dbbaaf94b6ee2a0c75
Nic <nic1@Dawns-MacBook-Air.local>
2023-04-23 11:57:42 -0400

    updated commit

--- roos_website/pages/_app.js
+++ roos_website/pages/_app.js
@@ -9,8 +9,7 @@ const MyApp = ({ Component, pageProps }) => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="preconnect" href="https://fonts.googleapis.com"></link>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-<link href="https://fonts.googleapis.com/css2?family=Gorditas:wght@400;700&display=swap" rel="stylesheet"></link>
-      </Head>
+        </Head>
     <Component {...pageProps} />
   </>
 );
